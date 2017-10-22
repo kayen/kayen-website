b7b68da87715611ade71d298394f457604ee08db
Ali Shanaakh <ashanaakh@gmail.com>
2017-10-22 20:10:42 +0300

    create Footer components & fix Navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5 col-sm-12 footer-bottom-left">
+          <p>Heliostech. &copy; 2017 All rights reserved.</p>
+        </div>
+        <div class="col-md-7 col-sm-12 footer-bottom-menu">
+          <ul class="list-unstyled">
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Customers</a></li>
+            <li><a href="">Support</a></li>
+            <li><a href="">New Features</a></li>
+          </ul>
+          <p id="back-top"><a href="#top"><i class="fa fa-angle-up"></i></a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Bottom'
+}
+</script>
