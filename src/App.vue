8e69bb99d82b66a8ce5278c32af01338be305d91
Ali Shanaakh <ashanaakh@gmail.com>
2017-10-22 14:33:19 +0300

    add styles & cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
+    <navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  }
 }
 </script>
+
+<style src="./assets/stylesheets/style.css"></style>
+<style src="./assets/stylesheets/colors/red.css"></style>
+<style src="./assets/stylesheets/responsive.css"></style>
+<style src="../node_modules/font-awesome/css/font-awesome.min.css"></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default">
+    <a class="navbar-brand" href="#">Kayen</a>
+    <button class="navbar-toggler" data-toggle="" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ">
+        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style src="../../node_modules/font-awesome/css/font-awesome.min.css"></style>
+<style src="../assets/stylesheets/style.css"></style>
+<style src="../assets/stylesheets/colors/red.css"></style>
+<style src="../assets/stylesheets/responsive.css"></style>
