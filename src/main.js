4b98910f9402849bb4ee8f8ca0b87162e84d939d
Louis Bellet <lbellet@heliostech.fr>
2017-10-23 00:30:48 +0300

    Adding normalize

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
 
 /* Bootstrap */
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import 'owl.carousel'
+import 'jquery-sticky'
+import 'magnific-popup'
+import 'counterup/jquery.counterup'
+import 'waypoints/lib/jquery.waypoints'
+import 'countdown/countdown'
+
+import '@/assets/js/custom'
+import '@/assets/js/plugins'
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import $ from 'jquery'
-
-import 'owl.carousel/dist/owl.carousel'
-import 'jquery-sticky/jquery.sticky'
-import 'magnific-popup/dist/jquery.magnific-popup'
-import 'counterup/jquery.counterup'
-import 'waypoints/lib/jquery.waypoints'
-import 'countdown/countdown'
-import Popper from 'popper.js'
-import 'bootstrap/dist/js/bootstrap'
-
-import '@/assets/js/custom'
-import '@/assets/js/plugins'
+// import $ from 'jquery'
 
 import LandingPage from '@/components/pages/landing'
 
-window.Popper = Popper
-window.jQuery = window.$ = $
+// window.Popper = Popper
+// window.jQuery = window.$ = $
 
 Vue.use(Router)
 
